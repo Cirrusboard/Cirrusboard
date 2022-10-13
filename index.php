ff9bd621521d79ef9e7e163deec123fade91e335
ROllerozxa <rollerozxa@voxelmanip.se>
2022-10-13 13:20:04 +0200

    Implement forum list

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 require('lib/common.php');
 
+$forums = query("SELECT f.* FROM forums f ORDER BY f.sort,f.id");
+
 $twig = twigloader();
 
 echo $twig->render('index.twig', [
+	'forums' => $forums,
 	'just_registered' => isset($_GET['rd'])
 ]);
--- sql/forum.sql
+++ sql/forum.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 10.9.3-MariaDB dump
+-- Adminer 4.8.1 MySQL 10.9.2-MariaDB dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -6,6 +6,17 @@ SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
+
+CREATE TABLE `forums` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Incrementing ID for every forum',
+  `sort` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Numeric value to sort forums in the wanted order, ascending',
+  `name` varchar(127) NOT NULL COMMENT 'Name of the forum',
+  `description` varchar(127) DEFAULT NULL COMMENT 'Description of the forum',
+  `threads` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'Total amount of threads in the forum',
+  `posts` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'Total amount of posts in the forum',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Incrementing ID for each user',
@@ -19,4 +30,4 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
--- 2022-10-08 14:49:05
+-- 2022-10-13 11:13:39
