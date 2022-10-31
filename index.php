2e02ab645d8511b1c87ef8ca982108e193789f33
ROllerozxa <rollerozxa@voxelmanip.se>
2022-10-31 17:24:14 +0100

    Add forum last post text

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $categs = query("SELECT id, title FROM categories ORDER BY ord, id");
 while ($c = $categs->fetch())
 	$categories[$c['id']] = $c['title'];
 
-$forums = query("SELECT f.* FROM forums f
+$forums = query("SELECT f.*, u.id u_id, u.name u_name, u.powerlevel u_powerlevel FROM forums f
+		LEFT JOIN users u ON u.id = f.lastuser
 		JOIN categories c ON c.id = f.cat
 		WHERE ? >= f.minread
 		ORDER BY c.ord, c.id, f.ord, f.id",
