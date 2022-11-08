3d854edbfbb836d2bf2e5839262416889217bbf6
ROllerozxa <rollerozxa@voxelmanip.se>
2022-11-08 20:11:24 +0100

    Implement latest thread post in forum list

--- forum.php
+++ forum.php
@@ -5,8 +5,10 @@ $id = $_GET['id'] ?? null;
 
 $forum = fetch("SELECT * FROM forums WHERE id = ?", [$id]);
 
-$threads = query("SELECT t.*, u.id u_id, u.name u_name, u.powerlevel u_powerlevel FROM threads t
+$threads = query("SELECT t.*, u.id u_id, u.name u_name, u.powerlevel u_powerlevel,
+		ul.id ul_id, ul.name ul_name, ul.powerlevel ul_powerlevel FROM threads t
 		JOIN users u ON u.id = t.user
+		JOIN users ul ON ul.id = t.lastuser
 		WHERE t.forum = ? ORDER BY t.id DESC",
 	[$id]);
 
