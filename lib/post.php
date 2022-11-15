1aa03addb739d86b681182ecae1c973d87bff228
ROllerozxa <rollerozxa@voxelmanip.se>
2022-11-15 12:34:05 +0100

    Swallow trailing newlines of quotes, makes quote pyramids pretties and makes me happier

--- lib/post.php
+++ lib/post.php
@@ -5,8 +5,6 @@ function postfilter($text) {
 
 	$text = trim($text);
 
-	$text = str_replace("\n", '<br>', $text);
-
 	// Basic formatting
 	$text = preg_replace("'\[b\](.*?)\[/b\]'si", '<b>\\1</b>', $text);
 	$text = preg_replace("'\[i\](.*?)\[/i\]'si", '<i>\\1</i>', $text);
@@ -24,7 +22,9 @@ function postfilter($text) {
 	$text = preg_replace("'\[quote\](.*?)\[/quote\]'si", '<div class="quote"><div class="quotetext">\\1</div></div>', $text);
 	$text = preg_replace("'\[quote=\"(.*?)\" id=\"(.*?)\"\]'si", '<div class="quote"><div class="author"><a href=thread.php?pid=\\2#\\2>Posted by \\1</a></div><div class="quotetext">', $text);
 	$text = preg_replace("'\[quote=(.*?)\]'si", '<div class="quote"><div class="author">Posted by ', $text);
-	$text = str_replace('[/quote]', '</div></div>', $text);
+	$text = preg_replace("'\[/quote\][\n\r\x0C]*'", '</div></div>', $text);
+
+	$text = str_replace("\n", '<br>', $text);
 
 	return $text;
 }
