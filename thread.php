ed66a275509b126330e01d017f777936ce5bf6d4
ROllerozxa <rollerozxa@voxelmanip.se>
2022-12-12 13:47:14 +0100

    Reduce repetition in thread queries

--- thread.php
+++ thread.php
@@ -19,10 +19,24 @@ elseif (isset($_GET['pid'])) { // Thing to ease permalinks, thread.php?pid=%d to
 } else
 	error('400', "I'm confused as to what you want...");
 
-$userpostfields = 'u.id u_id, u.name u_name, u.powerlevel u_powerlevel, '.postfields_user();
+// common fields to select
+$selectfields = 'u.id u_id, u.name u_name, u.powerlevel u_powerlevel, '.postfields_user().
+				'p.*, pt.text, pt.date, pt.revision cur_revision';
+
+// common joins
+$join = "FROM posts p
+		JOIN poststext pt ON p.id = pt.id AND p.revision = pt.revision
+		JOIN users u ON p.user = u.id
+		JOIN threads t ON p.thread = t.id
+		JOIN forums f ON f.id = t.forum";
+
+// search offset relative to the current page
 $offset = (($page - 1) * $ppp);
 
 if ($viewmode == 'thread') {
+
+	query("UPDATE threads SET views = views + 1 WHERE id = ?", [$id]);
+
 	$thread = fetch("SELECT t.*, f.title forum_title, f.id forum_id FROM threads t
 			JOIN forums f ON f.id = t.forum
 			WHERE t.id = ? AND ? >= f.minread",
@@ -30,10 +44,8 @@ if ($viewmode == 'thread') {
 
 	if (!$thread) error('404', "This forum doesn't exist.");
 
-	$posts = query("SELECT $userpostfields p.*, pt.text, pt.date ptdate, pt.revision cur_revision
-			FROM posts p
-			JOIN poststext pt ON p.id = pt.id AND p.revision = pt.revision
-			JOIN users u ON p.user = u.id
+	$posts = query("SELECT $selectfields
+			$join
 			WHERE p.thread = ?
 			ORDER BY p.id LIMIT ?,?",
 		[$id, $offset, $ppp]);
@@ -41,37 +53,33 @@ if ($viewmode == 'thread') {
 	$breadcrumb = ['forum.php?id='.$thread['forum_id'] => $thread['forum_title']];
 
 	$url = "thread.php?id=$id";
+
 } elseif ($viewmode == 'user') {
+
 	$user = fetch("SELECT name FROM users WHERE id = ?", [$uid]);
 
 	if (!$user) error('404', "This user doesn't exist.");
 
 	$thread['posts'] = result("SELECT COUNT(*) FROM posts WHERE user = ?", [$uid]);
 
-	$posts = query("SELECT $userpostfields p.*, pt.text, pt.date ptdate, pt.revision cur_revision, t.id tid, t.title ttitle
-			FROM posts p
-			LEFT JOIN poststext pt ON p.id = pt.id AND p.revision = pt.revision
-			LEFT JOIN users u ON p.user = u.id
-			LEFT JOIN threads t ON p.thread = t.id
-			LEFT JOIN forums f ON f.id = t.forum
+	$posts = query("SELECT $selectfields, t.id tid, t.title ttitle
+			$join
 			WHERE p.user = ? AND ? >= f.minread
-			ORDER BY p.id LIMIT ?,?",
+			ORDER BY p.date DESC LIMIT ?,?",
 		[$uid, $userdata['powerlevel'], $offset, $ppp]);
 
 	$breadcrumb = ['profile.php?id='.$uid => $user['name']];
 
 	$url = "thread.php?user=$uid";
+
 } elseif ($viewmode == 'time') {
+
 	$mintime = ($time > 0 && $time <= 2592000 ? time() - $time : 604800);
 
 	$thread['posts'] = result("SELECT COUNT(*) FROM posts WHERE date > ?", [$mintime]);
 
-	$posts = query("SELECT $userpostfields p.*, pt.text, pt.date ptdate, pt.revision cur_revision, t.id tid, t.title ttitle
-			FROM posts p
-			LEFT JOIN poststext pt ON p.id = pt.id AND p.revision = pt.revision
-			LEFT JOIN users u ON p.user = u.id
-			LEFT JOIN threads t ON p.thread = t.id
-			LEFT JOIN forums f ON f.id = t.forum
+	$posts = query("SELECT $selectfields, t.id tid, t.title ttitle
+			$join
 			WHERE p.date > ? AND ? >= f.minread
 			ORDER BY p.date DESC LIMIT ?,?",
 		[$mintime, $userdata['powerlevel'], $offset, $ppp]);
