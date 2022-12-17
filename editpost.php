cebc1de791ab41d40fce785e3a01d5830a8787e3
ROllerozxa <rollerozxa@voxelmanip.se>
2022-12-17 22:07:26 +0100

    Implement editing posts

---
+++ editpost.php
@@ -0,0 +1,70 @@
+<?php
+require('lib/common.php');
+
+needsLogin();
+
+$pid = $_GET['pid'] ?? null;
+$action = $_POST['action'] ?? null;
+
+$thread = fetch("SELECT p.user p_user, t.*, f.title f_title
+			FROM posts p
+			LEFT JOIN threads t ON t.id = p.thread
+			LEFT JOIN forums f ON f.id = t.forum
+			WHERE p.id = ? AND ? >= f.minread",
+		[$pid, $userdata['powerlevel']]);
+
+if (!$thread)
+	error('404', "Invalid post ID.");
+if ($thread['closed'] && $userdata['powerlevel'] <= 1)
+	error('403', "You can't edit a post in closed threads.");
+if ($userdata['powerlevel'] < 3 && $userdata['id'] != $thread['puser'])
+	error('403', "You do not have permission to edit this post.");
+
+$editpost = fetch("SELECT u.id, p.user, pt.text FROM posts p
+			LEFT JOIN poststext pt ON p.id = pt.id AND p.revision = pt.revision
+			LEFT JOIN users u ON p.user = u.id
+			WHERE p.id = ?",
+		[$pid]);
+
+$error = [];
+
+$message = $_POST['message'] ?? $editpost['text'];
+
+if ($action == 'Submit') {
+	if ($message == $post['text'])
+		$error[] = "No changes detected.";
+	if (strlen($message) < 15)
+		$error[] = "You can't blank out your post!";
+
+	if ($error == []) {
+		$newrev = result("SELECT revision FROM posts WHERE id = ?", [$pid]) + 1;
+
+		query("UPDATE posts SET revision = ? WHERE id = ?", [$newrev, $pid]);
+
+		query("INSERT INTO poststext (id, text, revision, date) VALUES (?,?,?,?)",
+			[$pid, $_POST['message'], $newrev, time()]);
+
+		redirect("thread.php?pid=$pid#$pid");
+	}
+} elseif ($action == 'Preview') {
+	$euser = fetch("SELECT * FROM users WHERE id = ?", [$editpost['id']]);
+	$post['date'] = time();
+	$post['text'] = $message;
+	foreach ($euser as $field => $val)
+		$post['u_'.$field] = $val;
+	$post['headerbar'] = 'Post preview';
+}
+
+$breadcrumb = [
+	'forum.php?id='.$thread['forum'] => $thread['f_title'],
+	'thread.php?id='.$thread['id'] => $thread['title']
+];
+
+echo twigloader()->render('editpost.twig', [
+	'thread' => $thread,
+	'post' => $post ?? null,
+	'action' => $action,
+	'pid' => $pid,
+	'message' => $message,
+	'breadcrumb' => $breadcrumb
+]);
