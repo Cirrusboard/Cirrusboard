7eb13949d18b27bea566b8f77dc57bf59546d162
ROllerozxa <rollerozxa@voxelmanip.se>
2023-08-15 16:50:47 +0200

    Refresh database schema

--- sql/forum.sql
+++ sql/forum.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 10.9.4-MariaDB dump
+-- Adminer 4.8.1 MySQL 10.11.3-MariaDB-1 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -8,9 +8,9 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 SET NAMES utf8mb4;
 
 CREATE TABLE `categories` (
-  `id` tinyint(3) unsigned NOT NULL COMMENT 'ID of the category',
-  `title` varchar(100) NOT NULL COMMENT 'Title of the category',
-  `ord` tinyint(3) unsigned NOT NULL COMMENT 'Display order of category, ascending',
+  `id` tinyint(3) unsigned NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `ord` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -19,19 +19,19 @@ INSERT INTO `categories` (`id`, `title`, `ord`) VALUES
 (2,	'Staff forums',	0);
 
 CREATE TABLE `forums` (
-  `id` int(10) unsigned NOT NULL COMMENT 'Incrementing ID for every forum',
-  `cat` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Category forum is attached to',
-  `ord` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Display order of forums, ascending',
-  `title` varchar(100) NOT NULL COMMENT 'Title of the forum',
-  `descr` varchar(200) DEFAULT NULL COMMENT 'Description of the forum',
-  `threads` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'Total amount of threads in the forum',
-  `posts` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'Total amount of posts in the forum',
-  `lastdate` int(10) unsigned DEFAULT NULL COMMENT 'Date of last post in forum',
-  `lastuser` int(10) unsigned DEFAULT NULL COMMENT 'Author of last post in forum',
-  `lastid` int(10) unsigned DEFAULT NULL COMMENT 'ID of last post in forum',
-  `minread` tinyint(4) NOT NULL DEFAULT -1 COMMENT 'Minimum rank to read/view forum',
-  `minthread` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'Minimum rank to make new threads',
-  `minreply` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'Minimum rank to reply to threads',
+  `id` int(10) unsigned NOT NULL,
+  `cat` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  `ord` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  `title` varchar(100) NOT NULL,
+  `descr` varchar(200) DEFAULT NULL,
+  `threads` int(10) unsigned NOT NULL DEFAULT 0,
+  `posts` int(10) unsigned NOT NULL DEFAULT 0,
+  `lastdate` int(10) unsigned DEFAULT NULL,
+  `lastuser` int(10) unsigned DEFAULT NULL,
+  `lastid` int(10) unsigned DEFAULT NULL,
+  `minread` tinyint(4) NOT NULL DEFAULT -1,
+  `minthread` tinyint(4) NOT NULL DEFAULT 1,
+  `minreply` tinyint(4) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   KEY `cat` (`cat`),
   CONSTRAINT `forums_ibfk_1` FOREIGN KEY (`cat`) REFERENCES `categories` (`id`)
@@ -53,22 +53,21 @@ CREATE TABLE `forumsread` (
 
 
 CREATE TABLE `guests` (
-  `ip` char(15) NOT NULL COMMENT 'The IP of the guest',
-  `lastview` int(10) unsigned NOT NULL COMMENT 'Timestamp of the guest''s lastview',
-  `bot` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Is guest a bot?',
-  `lastforum` int(10) unsigned DEFAULT NULL COMMENT 'Last forum guest visited (NYI)',
+  `ip` char(15) NOT NULL,
+  `lastview` int(10) unsigned NOT NULL,
+  `bot` tinyint(1) unsigned NOT NULL DEFAULT 0,
   UNIQUE KEY `ip` (`ip`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE `posts` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Incrementing ID for every post',
-  `user` int(10) unsigned NOT NULL COMMENT 'The user ID of the author.',
-  `thread` int(10) unsigned NOT NULL COMMENT 'The thread that this post is attached to.',
-  `date` int(10) unsigned NOT NULL COMMENT 'Date timestamp of post.',
-  `revision` smallint(5) unsigned NOT NULL DEFAULT 1 COMMENT 'The current text revision of the post.',
-  `ip` char(15) NOT NULL COMMENT 'IP address of the poster.',
-  `deleted` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Is post deleted?',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(10) unsigned NOT NULL,
+  `thread` int(10) unsigned NOT NULL,
+  `date` int(10) unsigned NOT NULL,
+  `revision` smallint(5) unsigned NOT NULL DEFAULT 1,
+  `ip` char(15) NOT NULL,
+  `deleted` tinyint(1) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `threadid` (`thread`),
   KEY `user` (`user`),
@@ -78,27 +77,27 @@ CREATE TABLE `posts` (
 
 
 CREATE TABLE `poststext` (
-  `id` int(10) unsigned NOT NULL COMMENT 'ID of the post',
-  `text` text NOT NULL COMMENT 'Teh text lol',
-  `revision` smallint(5) unsigned NOT NULL DEFAULT 1 COMMENT 'The revision of this post text',
-  `date` int(10) unsigned DEFAULT NULL COMMENT 'Date of the last revision',
+  `id` int(10) unsigned NOT NULL,
+  `text` text NOT NULL,
+  `revision` smallint(5) unsigned NOT NULL DEFAULT 1,
+  `date` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`,`revision`),
   CONSTRAINT `poststext_ibfk_2` FOREIGN KEY (`id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE `threads` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID of the thread',
-  `forum` int(10) unsigned NOT NULL COMMENT 'ID of the forum the thread is posted in',
-  `title` varchar(127) NOT NULL COMMENT 'Title of forum',
-  `user` int(10) unsigned NOT NULL COMMENT 'User ID of the forum author',
-  `posts` int(10) unsigned NOT NULL DEFAULT 1 COMMENT 'Amount of posts in the thread',
-  `views` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'Thread views',
-  `lastdate` int(10) unsigned DEFAULT NULL COMMENT 'Date of last post in thread',
-  `lastuser` int(10) unsigned DEFAULT NULL COMMENT 'Author of last post in thread',
-  `lastid` int(10) unsigned DEFAULT NULL COMMENT 'ID of last post in thread',
-  `closed` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Is thread closed/locked?',
-  `sticky` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Is thread sticky? (Shows up at the top of the thread listing)',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `forum` int(10) unsigned NOT NULL,
+  `title` varchar(127) NOT NULL,
+  `user` int(10) unsigned NOT NULL,
+  `posts` int(10) unsigned NOT NULL DEFAULT 1,
+  `views` int(10) unsigned NOT NULL DEFAULT 0,
+  `lastdate` int(10) unsigned DEFAULT NULL,
+  `lastuser` int(10) unsigned DEFAULT NULL,
+  `lastid` int(10) unsigned DEFAULT NULL,
+  `closed` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `sticky` tinyint(1) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `forum` (`forum`),
   KEY `user` (`user`),
@@ -124,39 +123,34 @@ CREATE TABLE `threadsread` (
 
 
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Incrementing ID for each user',
-  `name` varchar(32) NOT NULL COMMENT 'Username',
-  `password` char(60) NOT NULL COMMENT 'Password hash (bcrypt hashed)',
-  `token` char(64) NOT NULL COMMENT 'Token used for cookie authentication',
-  `ip` char(15) DEFAULT NULL COMMENT 'Latest IP address of user',
-  `url` varchar(150) DEFAULT NULL COMMENT 'Last URL user visited',
-  `joined` int(10) unsigned NOT NULL COMMENT 'Timestamp when user joined',
-  `lastview` int(10) unsigned DEFAULT NULL COMMENT 'Timestamp when user last viewed',
-  `lastpost` int(10) unsigned DEFAULT NULL COMMENT 'The ID of the user''s last post.',
-  `lastforum` int(10) unsigned DEFAULT NULL COMMENT 'ID of the forum user was last viewing.',
-  `rank` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'User''s rank, controlling access and permissions. (see perm.php)',
-  `tempbanned` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Is user tempbanned? (NYI)',
-  `posts` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'User''s amount of posts.',
-  `threads` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'User''s amount of threads.',
-  `rankset` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'The current rankset of the user (NYI)',
-  `blocklayouts` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Block all post layouts (signatures, headers etc)',
-  `signsep` tinyint(1) unsigned NOT NULL DEFAULT 1 COMMENT 'Signature separator',
-  `showemail` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether the user''s email should be visible.',
-  `ppp` tinyint(3) unsigned NOT NULL DEFAULT 20 COMMENT 'Posts per page for pagination',
-  `tpp` tinyint(3) unsigned NOT NULL DEFAULT 20 COMMENT 'Threads per page for pagination',
-  `theme` varchar(32) DEFAULT NULL COMMENT 'User''s theme (NYI)',
-  `email` varchar(100) DEFAULT NULL COMMENT 'User''s email address.',
-  `title` varchar(255) DEFAULT NULL COMMENT 'Custom user title.',
-  `avatar` tinyint(1) unsigned DEFAULT NULL COMMENT 'Does user have an avatar?',
-  `birthday` varchar(10) DEFAULT NULL COMMENT 'Birthday (in Y-M-d format)',
-  `customcolour` char(6) NOT NULL DEFAULT '000000' COMMENT 'Custom username colour',
-  `timezone` varchar(32) DEFAULT NULL COMMENT 'The user''s timezone',
-  `location` varchar(150) DEFAULT NULL COMMENT 'User''s location',
-  `header` text DEFAULT NULL COMMENT 'Post layout header',
-  `signature` text DEFAULT NULL COMMENT 'Post layout signature',
-  `bio` text DEFAULT NULL COMMENT 'Stuff to write that can be displayed on the user''s profile',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `password` char(60) NOT NULL,
+  `token` char(64) NOT NULL,
+  `ip` char(15) DEFAULT NULL,
+  `joined` int(10) unsigned NOT NULL,
+  `lastview` int(10) unsigned DEFAULT NULL,
+  `lastpost` int(10) unsigned DEFAULT NULL,
+  `rank` tinyint(4) NOT NULL DEFAULT 1,
+  `posts` int(10) unsigned NOT NULL DEFAULT 0,
+  `threads` int(10) unsigned NOT NULL DEFAULT 0,
+  `signsep` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  `showemail` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `ppp` tinyint(3) unsigned NOT NULL DEFAULT 20,
+  `tpp` tinyint(3) unsigned NOT NULL DEFAULT 20,
+  `theme` varchar(32) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `avatar` tinyint(1) unsigned DEFAULT NULL,
+  `birthday` varchar(10) DEFAULT NULL,
+  `customcolour` char(6) NOT NULL DEFAULT '000000',
+  `timezone` varchar(32) DEFAULT NULL,
+  `location` varchar(150) DEFAULT NULL,
+  `header` text DEFAULT NULL,
+  `signature` text DEFAULT NULL,
+  `bio` text DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
--- 2023-05-07 16:45:37
+-- 2023-08-15 14:51:09
